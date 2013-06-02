9cde6c2105abcca5c11c4649ea93362fa433f985
AlessioMTX <alessio.mitrotta@gmail.com>
2013-06-02 13:24:48 +0200

    Update mailer.php

--- contact-form/mailer.php
+++ contact-form/mailer.php
@@ -78,7 +78,7 @@ $headers	= 'MIME-Version: 1.0' . "\n";
 $headers	.= 'Content-type: text/html; charset=iso-8859-1' . "\n";
 $headers 	.= "From: $from";
 
-mail($to,$sbj,$msg,$headers); //Send principal mail.
+mail($emailto,$sbj,$msg,$headers); //Send principal mail.
 //End mail to admin
 
 //Start confermation mail
